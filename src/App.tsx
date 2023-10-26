ac7b45b3d226ca4244ff4b69966b4515943fa6b7
Irina <girishkaizh@gmail.com>
2023-10-26 23:53:06 +0400

    feat: add an error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Component } from 'react';
 
+import { ErrorBoundary } from '@/components/error-ui';
 import { SearchPage } from '@/pages/search-page';
 
 class App extends Component {
   render(): JSX.Element {
-    return <SearchPage />;
+    return (
+      <ErrorBoundary>
+        <SearchPage />
+      </ErrorBoundary>
+    );
   }
 }
 
---
+++ src/components/error-ui/error-boundary.tsx
@@ -0,0 +1,24 @@
+import { Component, ErrorInfo, PropsWithChildren } from 'react';
+
+class ErrorBoundary extends Component<PropsWithChildren> {
+  state = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(error, errorInfo.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
