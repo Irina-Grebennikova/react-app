c8785c482b3b87d578482e93e7b9c24a46cc8cc2
Irina <girishkaizh@gmail.com>
2023-11-03 22:58:56 +0400

    feat: add page 404

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+import dogImage from '@/assets/images/puppy.jpg';
+import { Button } from '@/components/ui';
+
+import styles from './not-found-page.module.scss';
+
+function NotFoundPage(): ReactNode {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.error}>
+        4
+        <img className={styles.image} src={dogImage} alt="" />4
+      </div>
+      <p className={styles.text}>Sorry, the page you are looking for does not exist.</p>
+      <Link to={'/'}>
+        <Button className={styles.button}>Back to search</Button>
+      </Link>
+    </div>
+  );
+}
+
+export { NotFoundPage };
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
+import { NotFoundPage } from '@/pages/not-found-page';
 import { SearchPage } from '@/pages/search-page';
 
-const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<SearchPage />} />));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<SearchPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 export { router };
