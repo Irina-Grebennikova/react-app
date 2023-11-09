0c2792d042c33a78fde25cdae735126ee38fad42
Irina <girishkaizh@gmail.com>
2023-11-10 01:23:18 +0400

    feat: set up a test environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,21 @@
 import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
+  setupFiles: ['jest-canvas-mock'],
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>'],
+  modulePaths: ['<rootDir>'],
+  setupFilesAfterEnv: ['<rootDir>/support/setup-tests.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  },
 };
 
 export default config;
