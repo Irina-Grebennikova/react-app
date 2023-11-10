10189df0d804a7ff7a7a1f39f42e5cb39089e176
Irina <girishkaizh@gmail.com>
2023-11-10 15:07:27 +0400

    test: add tests for the search component

---
+++ src/components/search/search.test.tsx
@@ -0,0 +1,69 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ReactElement, useState } from 'react';
+
+import { MockSearchPageContext } from '__mocks__/mock-search-page-context';
+
+import { KEY_PREFIX } from '@/helpers';
+
+import { Search } from './search';
+
+jest.mock('@/pages/search-page', () => ({
+  SearchPageContext: MockSearchPageContext,
+}));
+
+const getValueFromLocalStorage = (key: string): string | null => {
+  return localStorage.getItem(`${KEY_PREFIX}-${key}`);
+};
+
+function SearchProvider({ children }: { children: ReactElement }): ReactElement {
+  const [searchQuery, setSearchQuery] = useState(getValueFromLocalStorage('search-query') || '');
+
+  return (
+    <MockSearchPageContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        updateBreeds: jest.fn(),
+        showBreedsFromFirstPage: jest.fn(),
+      }}
+    >
+      {children}
+    </MockSearchPageContext.Provider>
+  );
+}
+
+describe('Search', () => {
+  it('clicking the Search button saves the entered value to the local storage', async () => {
+    render(
+      <SearchProvider>
+        <Search />
+      </SearchProvider>
+    );
+    const typedValue = 'pug';
+    const input = screen.getByRole('textbox');
+    const SearchButton = screen.getByRole('button', { name: /search/i });
+
+    await userEvent.type(input, typedValue);
+    await userEvent.click(SearchButton);
+
+    const savedValue = getValueFromLocalStorage('search-query');
+
+    expect(savedValue).toBe(typedValue);
+  });
+
+  it('component retrieves the value from the local storage upon mounting', () => {
+    const searchQuery = 'labrador';
+    localStorage.setItem(`${KEY_PREFIX}-search-query`, searchQuery);
+
+    render(
+      <SearchProvider>
+        <Search />
+      </SearchProvider>
+    );
+
+    const input = screen.getByRole('textbox');
+
+    expect(input).toHaveValue(searchQuery);
+  });
+});
